7d69a4c623475349d562e65820ff8fecc94ddbd7
sosen7874 <sosen7874@users.noreply.github.com>
2017-11-18 17:33:37 +0900

    22
    22

---
+++ src/Login.java
@@ -0,0 +1,14 @@
+
+
+public class Login {
+
+	public static void main(String[] args) {
+		String id = "testid";
+		String passwd = "1234";
+		if (id == "testid") {
+			if (passwd == "1234") {
+				System.out.println("로그인이 성공하였습니다!");
+			}
+		}
+	}
+}
